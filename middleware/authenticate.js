684b569acef3aff917031e81da53afab36e7e637
DAVID BARLOW <davidbarlow@DAVIDs-MacBook-Pro-2.local>
2019-03-13 10:47:49 -0700

    added x-auth to cors policy middleware

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -2,9 +2,7 @@ var {User} = require('./../models/user');
 
 var authenticate = (req,res,next) =>{
   var token = req.header('x-auth');
-    console.log('authenticate');
   User.findByToken(token).then((user) =>{
-    console.log('find by token', user);
       if(!user){
         return Promise.reject();
       }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -8,7 +8,7 @@ const {User} = require('../models/user');
 
 userRouter.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization,x-auth");
     res.header("Access-Control-Expose-Headers", "x-auth");
     //  res.header("Content-Type : text/plain");
     res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
